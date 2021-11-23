8fe8decb13d7390f444154adb25560562db43954
pallavi2193 <94918004+pallavi2193@users.noreply.github.com>
2021-11-23 21:07:20 +0530

    added code for given promotion

--- java/src/main/java/com/java/promotion/Promotion.java
+++ java/src/main/java/com/java/promotion/Promotion.java
@@ -2,5 +2,35 @@ package com.java.promotion;
 
 public class Promotion {
 		
+	public int checkForPromotion(Map<String, Integer> products) {
+		
+		
+		if(products.containsKey("A")) {
+			while(products.get("A") >= 3) {
+				promotionValue = promotionValue + 130;
+				value = products.get("A") - 3;
+				products.put("A", value);
+			}
+		}
+		if(products.containsKey("B")) {
+			while(products.get("B") >= 2) {
+				promotionValue = promotionValue + 45;
+				value = products.get("B") - 2;
+				products.put("B", value);
+			}
+		}
+		if(products.containsKey("C") && products.containsKey("D")) {
+			while(products.get("C") >= 1 && products.get("D") >= 1) {
+				promotionValue = promotionValue + 30;
+				value = products.get("C") - 1;
+				products.put("C", value);
+				value = products.get("D") - 1;
+				products.put("D", value);
+			}
+		}
+		
+		return promotionValue;
+		
+	}
 	}
 }
