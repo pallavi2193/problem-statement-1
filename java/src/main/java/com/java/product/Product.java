8eb9d65d98451cf166f7dcfdc7c153d42d08234a
pallavi2193 <94918004+pallavi2193@users.noreply.github.com>
2021-11-23 21:12:06 +0530

    Update Product.java

--- java/src/main/java/com/java/product/Product.java
+++ java/src/main/java/com/java/product/Product.java
@@ -1,6 +1,31 @@
 package com.java.product;
 
+import java.util.Map;
+
 public class Product {
 
+  private int price = 0;
+  
+  public int calculateProducts(Map<String, Integer> products) {
+		 
+		for(Map.Entry<String, Integer> set : products.entrySet()) {
+			switch(set.getKey()) {
+			case "A": 
+				price = price + products.get("A") * 50;
+				break;
+			case "B": 
+				price = price + products.get("B") * 30;
+				break;
+			case "C": 
+				price = price + products.get("C") * 20;
+				break;
+			case "D": 
+				price = price + products.get("D") * 15;
+				break;
+			}
+		}
+		
+		return price;
+	}
 
 }
