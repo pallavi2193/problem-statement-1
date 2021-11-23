597b72f639347405270a20ca3f82e99f1c2fe996
pallavi2193 <94918004+pallavi2193@users.noreply.github.com>
2021-11-23 21:05:22 +0530

    Update Application.java

--- java/src/main/java/com/java/app/Application.java
+++ java/src/main/java/com/java/app/Application.java
@@ -8,7 +8,8 @@ import com.java.promotion.Promotion;
 public class Application {
 
 	public static void main(String[] args) {
-
+		Map<String, Integer> products = new HashMap<>();
+		checkForPromotion(products);
 	}
 
 }
